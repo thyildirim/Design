1b1ea51294b9f4e72f222e7a91488d47bafa06e5
FarukErat <farukerat1327@gmail.com>
2025-01-03 18:14:50 +0300

    add swagger

--- gencryption/urls.py
+++ gencryption/urls.py
@@ -16,7 +16,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Gencryption API",
+        default_version='v1',
+        description="API documentation for Gencryption",
+        terms_of_service="https://www.gencryption.com/terms/",
+        contact=openapi.Contact(email="support@gencryption.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0)),
 ]
