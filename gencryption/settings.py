1cb21be9941c3841d26ab1dbe47877fc9fff3a81
FarukErat <farukerat1327@gmail.com>
2025-01-03 21:55:23 +0300

    fix csrf issue

--- gencryption/settings.py
+++ gencryption/settings.py
@@ -13,6 +13,15 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'gencryption.config.CsrfExemptSessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
