2dd8515355b9c765213c0f9ac87fe22554d7ca11
FarukErat <farukerat1327@gmail.com>
2025-01-03 21:38:30 +0300

    define DEFAULT_AUTHENTICATION_CLASSES

--- gencryption/settings.py
+++ gencryption/settings.py
@@ -13,6 +13,13 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+    ),
+}
+
+
 APPEND_SLASH = False
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -40,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'drf_yasg',
     'accounts',
 ]
@@ -48,7 +56,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
